892a69dae679d276856b802bee292d5491dd5e8e
WalterTNFC <walter.tad@hotmail.com>
2022-08-04 10:11:06 -0300

    feat: Requisito 3/ Using package crypto to generate  a randon token

--- middlewares.js/helpers.js
+++ middlewares.js/helpers.js
@@ -1,9 +1,16 @@
+const crypto = require('crypto');
 const fs = require('fs').promises;
 
 async function readJson(jsonTalker) {
   return JSON.parse(await fs.readFile(jsonTalker));
 }
 
+// Referencia: https://stackoverflow.com/questions/8855687/secure-random-token-in-node-js
+function generateToken() {
+  return crypto.randomBytes(8).toString('hex');
+}
+
 module.exports = {
   readJson,
+  generateToken,
 };
