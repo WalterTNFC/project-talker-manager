9fa80bb1678f2c32e4bd19493e4c252cf03fbc01
WalterTNFC <walter.tad@hotmail.com>
2022-08-04 08:51:09 -0300

    feat: create route to get all talkers

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { getTalker } = require('./middlewares.js/requests');
 
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+// Requisito 1:
+app.get('/talker', getTalker);
 
 // não remova esse endpoint, e para o avaliador funcionar!
 app.get('/', (_request, response) => {
