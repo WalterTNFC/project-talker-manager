11a9a587b06f4d659a3ffdca22552767af372bd3
WalterTNFC <walter.tad@hotmail.com>
2022-08-04 09:22:52 -0300

    feat: Requisito 2/ function getTalkerById

--- middlewares.js/requests.js
+++ middlewares.js/requests.js
@@ -2,11 +2,24 @@ const { readJson } = require('./helpers');
 
 const jsonTalker = './talker.json';
 
+// Requisito 1:
 async function getTalker(_req, res) {
-  const talker = await readJson(jsonTalker);
-  return res.status(200).json(talker);
+  const talkers = await readJson(jsonTalker);
+  return res.status(200).json(talkers);
+}
+
+// Requisito 2:
+async function getTalkerById(req, res) {
+  const { id } = req.params;
+  const talkers = await readJson(jsonTalker);
+  const talkerById = talkers.find((value) => value.id === +id);
+  if (!talkerById) {
+    return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  }
+  return res.status(200).json(talkerById);
 }
 
 module.exports = {
   getTalker,
+  getTalkerById,
 };
