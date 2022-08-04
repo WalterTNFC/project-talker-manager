d2dcda0069c8fb9494c6d96514ed401574da173a
WalterTNFC <walter.tad@hotmail.com>
2022-08-04 12:14:13 -0300

    feat: Requisito 4/ Function getAndValidateToken

--- middlewares.js/requests.js
+++ middlewares.js/requests.js
@@ -19,18 +19,29 @@ async function getTalkerById(req, res) {
   return res.status(200).json(talkerById);
 }
 
-// Requisito 3
-async function handleLogin(req, res) {
+// Requisito 3 e 4
+async function getAndValidateToken(req, res) {
   const { email, password } = req.body;
-  const token = generateToken();
-  if (!email || !password) {
-    return res.status(400).json({ message: 'Email ou senha não cadastrados' });
+
+  if (!email) {
+    return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   }
+  if (!password) {
+    return res.status(400).json({ message: 'O campo "password" é obrigatório' });
+  }
+  if (!(email.match(/^[^\s@!#$%"'&*()]+@[^\s@!#$%"'&*()]+\.com+$/))) {
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
+  }
+  if (password.length < 6) {
+    return res.status(400).json({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+  }
+  
+  const token = generateToken();
   return res.status(200).json({ token });
 }
 
 module.exports = {
   getTalker,
   getTalkerById,
-  handleLogin,
+  getAndValidateToken,
 };
